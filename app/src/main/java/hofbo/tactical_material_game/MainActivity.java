8dca50e2d70396b3d77de48d1a0104e4be6b5b98
Deniz Hofmann <deniz-hofmann@outlook.com>
2017-10-06 09:24:56 +0200

    Fragments jetzt drinnen :D

--- app/src/main/java/hofbo/tactical_material_game/MainActivity.java
+++ app/src/main/java/hofbo/tactical_material_game/MainActivity.java
@@ -1,30 +1,40 @@
 package hofbo.tactical_material_game;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, LoadoutFragment.OnFragmentInteractionListener, AccountFragment.OnFragmentInteractionListener{
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+
+        FragmentManager fragmentManager = getFragmentManager();
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction  = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.navigation_settings);
+                    transaction.replace(R.id.Fragment_Container, new LoadoutFragment());
+                    transaction.commit();
                     return true;
                 case R.id.navigation_highscore:
-                    mTextMessage.setText(R.string.navigation_highscore);
+                    transaction.add(R.id.Fragment_Container,new LoginFragment());
+                    transaction.commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    transaction.replace(R.id.Fragment_Container,new AccountFragment());
+                    transaction.commit();
                     return true;
             }
             return false;
@@ -37,9 +47,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
