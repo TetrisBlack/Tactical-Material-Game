a7f297af9ac7ca96d683baf63979e3b17b2512d3
root <deniz-hofmann@outlook.com>
2017-10-14 03:44:41 +0200

    Alot Sign up etc -> Fertig

--- app/src/main/java/hofbo/tactical_material_game/AccountFragment.java
+++ app/src/main/java/hofbo/tactical_material_game/AccountFragment.java
@@ -2,23 +2,46 @@ package hofbo.tactical_material_game;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Picture;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 /**
@@ -29,10 +52,13 @@ import com.google.firebase.auth.FirebaseAuth;
  * Use the {@link AccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountFragment extends Fragment {
+public class AccountFragment extends Fragment implements
+        GoogleApiClient.OnConnectionFailedListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_FIREBASEAUTH = "FIREBASEAUTH";
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
 
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
@@ -47,6 +73,9 @@ public class AccountFragment extends Fragment {
                     break;
                 case R.id.btn_logout:
                     logout();
+                    break;
+                case R.id.btn_login_with_google:
+                    signIn();
                     break;
 
             }
@@ -67,7 +96,6 @@ public class AccountFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
      * @return A new instance of fragment AccountFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -85,23 +113,99 @@ public class AccountFragment extends Fragment {
 
 
         }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
         mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+
+        if (mAuth.getCurrentUser() != null) {
+            updateUI(true, getView(), mAuth);
+
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
-        Button registerButton = (Button) view.findViewById(R.id.btn_register);
-        Button loginButton = (Button) view.findViewById(R.id.btn_login);
-        Button logoutButton = (Button) view.findViewById(R.id.btn_logout);
 
-        logoutButton.setOnClickListener(mOnClickListener);
-        loginButton.setOnClickListener(mOnClickListener);
-        registerButton.setOnClickListener(mOnClickListener);
+
+        view.findViewById(R.id.btn_register).setOnClickListener(mOnClickListener);
+        view.findViewById(R.id.btn_login).setOnClickListener(mOnClickListener);
+        view.findViewById(R.id.btn_logout).setOnClickListener(mOnClickListener);
+        view.findViewById(R.id.btn_login_with_google).setOnClickListener(mOnClickListener);
+
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
 
@@ -112,7 +216,7 @@ public class AccountFragment extends Fragment {
         String email = login_email.getText().toString();
         String password = login_password.getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(email,password)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -131,8 +235,9 @@ public class AccountFragment extends Fragment {
                     }
                 });
     }
+
     //Login
-    public void login (){
+    public void login() {
         EditText login_email = (EditText) getActivity().findViewById(R.id.login_email);
         EditText login_password = (EditText) getActivity().findViewById(R.id.login_password);
         String email = login_email.getText().toString();
@@ -162,11 +267,84 @@ public class AccountFragment extends Fragment {
     }
 
     //Logout
-    public void logout(){
+    public void logout() {
         mAuth.signOut();
     }
 
+    //Update UI
+    public static void updateUI(final boolean signedIn, final View v, final FirebaseAuth mAut) {
+        if (v.findViewById(R.id.cardAccount) != null) {
+            if (signedIn) {
+                TextView tmp;
+                tmp = v.findViewById(R.id.lbl_email_user);
+                tmp.setText(mAut.getCurrentUser().getEmail());
 
+                tmp = v.findViewById(R.id.lbl_name_user);
+                tmp.setText(mAut.getCurrentUser().getDisplayName());
+
+                tmp = v.findViewById(R.id.lbl_uid_user);
+                tmp.setText(mAut.getCurrentUser().getUid());
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        // a potentially  time consuming task
+                        try {
+                            final Bitmap uProfilePic = BitmapFactory.decodeStream((InputStream) new URL(mAut.getCurrentUser().getPhotoUrl().toString()).getContent());
+                            ImageView tmpPic = v.findViewById(R.id.imgv_profile_pic);
+                            tmpPic.post(new Runnable() {
+                                public void run() {
+                                    ImageView tmpPic = v.findViewById(R.id.imgv_profile_pic);
+                                    tmpPic.setImageBitmap(uProfilePic);
+                                }
+                            });
+                        } catch (MalformedURLException e) {
+
+
+                        } catch (IOException e) {
+
+                        } catch (Exception e){
+
+                        }
+
+                    }
+                }).start();
+
+
+            } else {
+                TextView tmp;
+                tmp = v.findViewById(R.id.lbl_email_user);
+                tmp.setText("");
+
+                tmp = v.findViewById(R.id.lbl_name_user);
+                tmp.setText("");
+
+                tmp = v.findViewById(R.id.lbl_uid_user);
+                tmp.setText("");
+
+                ImageView tmpPic = v.findViewById(R.id.imgv_profile_pic);
+                tmpPic.setImageResource(R.drawable.common_google_signin_btn_icon_dark);
+
+            }
+        }
+
+    }
+
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+
+        }
+        return bm;
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -183,6 +361,11 @@ public class AccountFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     /**
--- app/src/main/java/hofbo/tactical_material_game/MainActivity.java
+++ app/src/main/java/hofbo/tactical_material_game/MainActivity.java
@@ -1,5 +1,8 @@
 package hofbo.tactical_material_game;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -15,21 +18,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity implements
         LoadoutFragment.OnFragmentInteractionListener, Highscore_Fragment.OnFragmentInteractionListener,
         Game_Fragment.OnFragmentInteractionListener, NewsFragment.OnFragmentInteractionListener,
-        AccountFragment.OnFragmentInteractionListener{
+        AccountFragment.OnFragmentInteractionListener {
 
     //Firebase Auth
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    public Bitmap uProfilePic;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,11 +59,11 @@ public class MainActivity extends AppCompatActivity implements
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            FragmentTransaction transaction  = fragmentManager.beginTransaction();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_settings:
 
-                    transaction.replace(R.id.Fragment_Container, new AccountFragment().newInstance(mAuth));
+                    transaction.replace(R.id.Fragment_Container, new AccountFragment());
                     transaction.commit();
                     return true;
                 case R.id.navigation_highscore:
@@ -52,15 +72,15 @@ public class MainActivity extends AppCompatActivity implements
                     transaction.commit();
                     return true;
                 case R.id.navigation_start_game:
-                    transaction.replace(R.id.Fragment_Container,new Game_Fragment());
+                    transaction.replace(R.id.Fragment_Container, new Game_Fragment());
                     transaction.commit();
                     return true;
                 case R.id.navigation_loadout:
-                    transaction.replace(R.id.Fragment_Container,new LoadoutFragment());
+                    transaction.replace(R.id.Fragment_Container, new LoadoutFragment());
                     transaction.commit();
                     return true;
                 case R.id.navigation_news:
-                    transaction.replace(R.id.Fragment_Container,new NewsFragment());
+                    transaction.replace(R.id.Fragment_Container, new NewsFragment());
                     transaction.commit();
                     return true;
             }
@@ -73,20 +93,26 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         //Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
+
+
+                    AccountFragment.updateUI(true,findViewById(R.id.Fragment_Container),mAuth);
                     Snackbar snackbar = Snackbar
-                            .make(findViewById(R.id.Fragment_Container), "Willkommen zurück " + user.getDisplayName(), Snackbar.LENGTH_LONG);
+                            .make(findViewById(R.id.Fragment_Container), "Willkommen zurück " + user.getDisplayName(), Snackbar.LENGTH_SHORT);
 
                     snackbar.show();
                 } else {
                     // User is signed out
+                    AccountFragment.updateUI(false,findViewById(R.id.Fragment_Container),mAuth);
                     Snackbar snackbar = Snackbar
                             .make(findViewById(R.id.Fragment_Container), "Logged Out", Snackbar.LENGTH_LONG);
 
@@ -97,27 +123,51 @@ public class MainActivity extends AppCompatActivity implements
         };
 
 
-
-
         setContentView(R.layout.activity_main);
 
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_start_game);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 
 
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
+
     @Override
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
     @Override
     public void onStop() {
         super.onStop();
